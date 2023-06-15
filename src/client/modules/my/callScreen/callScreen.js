d5ee8c7c2cb300ca6c67a6d1fc424e4d5767a1df
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-15 14:42:57 +0300

    test

--- src/client/modules/my/callScreen/callScreen.js
+++ src/client/modules/my/callScreen/callScreen.js
@@ -1,13 +1,37 @@
-import {LightningElement} from "lwc";
+import {LightningElement} from 'lwc';
+import PORTRAITS from './charactersToPortraits.json'
+import IDENTIFIERS from './ruCharactersToIdenfitiers.json';
 
 export default class CallScreen extends LightningElement {
+
+    scene1 = [];
 
     async connectedCallback() {
         await fetch('https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/callScreen/scene-1.txt').
         then(response => {
             return response.text()
         }).then(text => {
-            console.log(text);
+            this.parseScene(text);
         });
+    }
+
+    parseScene(text) {
+        console.log(text);
+        let lines = text.split('\n');
+        let i = 1;
+        lines.forEach(line => {
+           this.scene1.push(this.parseLine(line));
+        });
+        console.log(this.scene1);
+    }
+
+    parseLine(line) {
+        let [identifier, ...text] = line.split(':');
+        identifier = IDENTIFIERS[identifier];
+        console.log(identifier);
+        return {
+            speaker: identifier,
+            line: text
+        };
     }
 }
