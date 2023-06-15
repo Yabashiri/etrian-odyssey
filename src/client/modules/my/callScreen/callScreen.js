9f943ca6b07e4f185c8ed113666e82633f83f005
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-15 13:21:41 +0300

    test

--- src/client/modules/my/callScreen/callScreen.js
+++ src/client/modules/my/callScreen/callScreen.js
@@ -4,13 +4,7 @@ export default class CallScreen extends LightningElement {
 
     async connectedCallback() {
         await fetch('scene1-txt').then(response => {
-            console.log(response);
-            console.log('test')
-        }).catch(error => {
-            console.log(error);
-        });
-        await fetch('./scene1-txt').then(response => {
-            console.log(response);
+            console.log(response.text());
             console.log('test')
         }).catch(error => {
             console.log(error);
