f5c7a333aba669250722552834e9262aba161ad8
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-15 14:12:33 +0300

    test

--- src/client/modules/my/callScreen/callScreen.js
+++ src/client/modules/my/callScreen/callScreen.js
@@ -3,18 +3,11 @@ import {LightningElement} from "lwc";
 export default class CallScreen extends LightningElement {
 
     async connectedCallback() {
-        await fetch('https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/callScreen/scene-1.txt').then(response => {
-            console.log(response.text());
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
-        });
-
-        await fetch('https://example-files.online-convert.com/document/txt/example.txt').then(response => {
-            console.log(response.text());
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
+        await fetch('https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/callScreen/scene-1.txt').
+        then(response => {
+            return response.text()
+        }).then(text => {
+            console.log(text);
         });
     }
 }
