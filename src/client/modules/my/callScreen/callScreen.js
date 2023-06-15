a3e0f6f27fd96285d4aee2b04630f2e94a83cd80
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-15 13:19:37 +0300

    test

--- src/client/modules/my/callScreen/callScreen.js
+++ src/client/modules/my/callScreen/callScreen.js
@@ -3,7 +3,13 @@ import {LightningElement} from "lwc";
 export default class CallScreen extends LightningElement {
 
     async connectedCallback() {
-        await fetch('https://example-files.online-convert.com/document/txt/example.txt').then(response => {
+        await fetch('scene1-txt').then(response => {
+            console.log(response);
+            console.log('test')
+        }).catch(error => {
+            console.log(error);
+        });
+        await fetch('./scene1-txt').then(response => {
             console.log(response);
             console.log('test')
         }).catch(error => {
