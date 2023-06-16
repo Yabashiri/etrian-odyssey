64e58e88297b13bec1658ab105e2dd760f367d71
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-16 13:17:40 +0300

    finish renaming

--- src/client/modules/my/chapterOne/chapterOne.js
+++ src/client/modules/my/chapterOne/chapterOne.js
@@ -16,7 +16,7 @@ export default class CallScreen extends LightningElement {
     }
 
     async connectedCallback() {
-        await fetch('https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/callScreen/scene-1.txt').
+        await fetch('https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/chapterOne/scene.txt').
         then(response => {
             return response.text()
         }).then(text => {
