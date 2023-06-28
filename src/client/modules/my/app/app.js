e90fc6595fc03c888f80b89207ca3043a717ff37
Yana Krasnostavskaya <y.krasnostavskaya@gbc-team.com>
2023-06-28 16:45:30 +0300

    конец главы 3

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -13,7 +13,7 @@ export default class App extends LightningElement {
             case 'floors12':
                 this.chapter = 'https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/chapterOne/scenes/scene-2.txt';
                 break;
-            case 'floors34':
+            case 'floors35':
                 this.chapter = 'https://raw.githubusercontent.com/Yabashiri/etrian-odyssey/master/src/client/modules/my/chapterOne/scenes/scene-3.txt';
                 break;
             default:
